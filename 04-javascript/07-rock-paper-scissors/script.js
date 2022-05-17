e50adefae8aac50de323d85c1362e7ca77dbc1d3
robAnderson <reanderson89@gmail.com>
2022-05-17 12:05:59 -0700

    added text

--- 04-javascript/07-rock-paper-scissors/script.js
+++ 04-javascript/07-rock-paper-scissors/script.js
@@ -1,3 +1,4 @@
+// First hint
 let wins = 0;
 let losses = 0;
 let ties = 0;
@@ -9,6 +10,8 @@ const getUserChoice = function(){
 
 getUserChoice();
 
+
+// Second hint
 const getComputerChoice = function(){
     let randomNum = Math.floor(Math.random() * 3);
     let computerChoice;
