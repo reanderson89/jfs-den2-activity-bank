e4b4807c6176b4eecde0a36292703bd2f1f5e6d1
robAnderson <reanderson89@gmail.com>
2022-05-17 12:15:02 -0700

    WIP

--- 04-javascript/07-rock-paper-scissors/script.js
+++ 04-javascript/07-rock-paper-scissors/script.js
@@ -8,7 +8,6 @@ const getUserChoice = function(){
     return userChoice;
 }
 
-getUserChoice();
 
 
 // Second hint
@@ -24,6 +23,34 @@ const getComputerChoice = function(){
     }
     return computerChoice;
 }
+
+
+// third hint
+const playGame = function(){
+    let userChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
+
+    if ((userChoice === "r" && computerChoice === "s") ||
+            (userChoice === "s" && computerChoice === "p") || 
+            (userChoice === "p" && computerChoice === "r")) {
+            wins++;
+            alert("You've won " + wins + " time(s)!");
+          } else if (userChoice === computerChoice) {
+            ties++;
+            alert("You've tied " + ties + " time(s).");
+          } else {
+            losses++;
+            alert("You've lost " + losses + " time(s).");
+          }
+
+    let playAgain = confirm("Do you want to play again?");
+
+    if(playAgain === true){
+        playGame();
+    }
+}
+
+playGame();
 
 
 
