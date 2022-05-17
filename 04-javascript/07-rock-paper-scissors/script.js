88cafdf37b7d36aeea77412897079526759cacfe
robAnderson <reanderson89@gmail.com>
2022-05-17 12:05:24 -0700

    first and second hint

---
+++ 04-javascript/07-rock-paper-scissors/script.js
@@ -0,0 +1,27 @@
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+const getUserChoice = function(){
+    let userChoice = prompt("please choose 'r' for rock, 'p' for paper, or 's' for scissors");
+    return userChoice;
+}
+
+getUserChoice();
+
+const getComputerChoice = function(){
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice;
+    if(randomNum === 0){
+        computerChoice = "r";
+    } else if (randomNum === 1){
+        computerChoice = "s"
+    } else if (randomNum === 2){
+        computerChoice = "p"
+    }
+    return computerChoice;
+}
+
+
+
+
