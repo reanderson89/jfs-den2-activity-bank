d4459456d466b4b4881cecf6b8dc467f7438638c
robAnderson <reanderson89@gmail.com>
2022-05-19 10:42:20 -0700

    edited tests

--- 04-javascript/08-loops/challenge-1/script.js
+++ 04-javascript/08-loops/challenge-1/script.js
@@ -1,13 +1,4 @@
-
-
-
-
-
-
-
-
-
-
+// create your function here and name it addNumbers
 
 
 
@@ -25,15 +16,15 @@
 // When you are done with your function uncomment the function calls below to test your code
 
 // these should fail and print "Invalid Number"
-console.log(addNumbers(5))
-console.log(addNumbers(1200))
+    // console.log(addNumbers(5));
+    // console.log(addNumbers(1200));
 
 
 // These should pass and print the value of the numbers added together
-console.log(917);
-console.log(503);
-console.log(404);
-console.log(541);
-console.log(78);
-console.log(26);
-console.log(238);
+    // console.log(addNumbers(917));
+    // console.log(addNumbers(503));
+    // console.log(addNumbers(404));
+    // console.log(addNumbers(541));
+    // console.log(addNumbers(78));
+    // console.log(addNumbers(26));
+    // console.log(addNumbers(238));
