0a2da650cc2fa7c38494795dbada0e54c8353ec6
robAnderson <reanderson89@gmail.com>
2022-05-19 10:40:14 -0700

    added tests

---
+++ 04-javascript/08-loops/challenge-1/script.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// When you are done with your function uncomment the function calls below to test your code
+
+// these should fail and print "Invalid Number"
+console.log(addNumbers(5))
+console.log(addNumbers(1200))
+
+
+// These should pass and print the value of the numbers added together
+console.log(917);
+console.log(503);
+console.log(404);
+console.log(541);
+console.log(78);
+console.log(26);
+console.log(238);
