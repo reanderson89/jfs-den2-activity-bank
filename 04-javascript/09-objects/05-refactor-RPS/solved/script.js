5c5c245dce9b9f2e610a21bfbaf203c856c221f9
robAnderson <reanderson89@gmail.com>
2022-05-26 12:49:53 -0700

    done

--- 04-javascript/09-objects/05-refactor-RPS/solved/script.js
+++ 04-javascript/09-objects/05-refactor-RPS/solved/script.js
@@ -1,16 +1,13 @@
-// First hint
-// let wins = 0;
-// let losses = 0;
-// let ties = 0;
-
 const userObj = {
+    name: prompt("What is your name?"),
     wins: 0,
     losses: 0,
     ties: 0,
-    choice: "",
+    _choice: "",
     askUserChoice(){
         let userChoice = prompt("please choose 'r' for rock, 'p' for paper, or 's' for scissors");
-        this.choice = userChoice; 
+        this._choice = userChoice; 
+        return userChoice;
     }
 }
 
@@ -18,7 +15,7 @@ const compObj = {
     wins: 0,
     losses: 0,
     ties: 0,
-    choice: "",
+    _choice: "",
     generateCompChoice(){
         let randomNum = Math.floor(Math.random() * 3);
         let compChoice;
@@ -30,22 +27,17 @@ const compObj = {
             compChoice = "p"
         }
 
-        this.choice = compChoice; 
+        this._choice = compChoice; 
+
+        return compChoice;
     }
 }
 
 const gameObj = {
-    userStandings: {
-        wins: userObj.wins,
-        losses: userObj.losses,
-        ties: userObj.ties
-    },
-    compStandings: {
-        wins: compObj.wins,
-        losses: compObj.losses,
-        ties: compObj.ties
-    },
+    gameCounter:1,
+    gamesToPlay: prompt("How many games would you like to play?"),
     playGame(){
+        this.gameCounter++;
         let userChoice = userObj.askUserChoice();
     let computerChoice = compObj.generateCompChoice();
     alert(`You chose: ${userChoice}\nComputer chose: ${computerChoice}`)
@@ -53,83 +45,36 @@ const gameObj = {
     if ((userChoice === "r" && computerChoice === "s") ||
             (userChoice === "s" && computerChoice === "p") || 
             (userChoice === "p" && computerChoice === "r")) {
-            wins++;
-            alert("You've won " + wins + " time(s)!");
+            userObj.wins++;
+            compObj.losses++;
+            alert("You won!")
           } else if (userChoice === computerChoice) {
-            ties++;
-            alert("You've tied " + ties + " time(s).");
+            userObj.ties++;
+            compObj.ties++;
+            alert("You tied...")
           } else {
-            losses++;
-            alert("You've lost " + losses + " time(s).");
+            userObj.losses++;
+            compObj.wins++;
+            alert("womp womp womp...")
           }
 
-        //   4th hint
-    alert(`Current Standings:\nWins: ${wins}\nLosses: ${losses}\n Ties: ${ties}`)
-
-    let playAgain = confirm("Do you want to play again?");
-    
-    let totalPlays = wins + losses + ties;
-
-    if(playAgain === true && totalPlays < 10){
-        playGame();
+    if(this.gameCounter <= this.gamesToPlay){
+        this.playGame();
+    } else{
+        alert(`Final Standings:
+    ${userObj.name} won ${userObj.wins} time(s) and the computer won ${compObj.wins} time(s).
+    ${userObj.name} lost ${userObj.losses} time(s) and the computer lost ${compObj.losses} time(s).
+    There was ${userObj.ties} tie(s).
+    ${(userObj.wins > compObj.wins ? `${userObj.name} Wins!`: `The Computer Wins!`)}`)
     }
     }
 }
 
-console.log(gameObj.compStandings.wins);
-
-// const getUserChoice = function(){
-//     let userChoice = prompt("please choose 'r' for rock, 'p' for paper, or 's' for scissors");
-//     return userChoice;
-// }
+gameObj.playGame()
 
 
 
-// Second hint
-// const getComputerChoice = function(){
-//     let randomNum = Math.floor(Math.random() * 3);
-//     let computerChoice;
-//     if(randomNum === 0){
-//         computerChoice = "r";
-//     } else if (randomNum === 1){
-//         computerChoice = "s"
-//     } else if (randomNum === 2){
-//         computerChoice = "p"
-//     }
-//     return computerChoice;
-// }
 
-
-// third hint
-const playGame = function(){
-    let userChoice = getUserChoice();
-    let computerChoice = getComputerChoice();
-    alert(`You chose: ${userChoice}\nComputer chose: ${computerChoice}`)
-
-    if ((userChoice === "r" && computerChoice === "s") ||
-            (userChoice === "s" && computerChoice === "p") || 
-            (userChoice === "p" && computerChoice === "r")) {
-            wins++;
-            alert("You've won " + wins + " time(s)!");
-          } else if (userChoice === computerChoice) {
-            ties++;
-            alert("You've tied " + ties + " time(s).");
-          } else {
-            losses++;
-            alert("You've lost " + losses + " time(s).");
-          }
-
-        //   4th hint
-    alert(`Current Standings:\nWins: ${wins}\nLosses: ${losses}\n Ties: ${ties}`)
-
-    let playAgain = confirm("Do you want to play again?");
-    
-    let totalPlays = wins + losses + ties;
-
-    if(playAgain === true && totalPlays < 10){
-        playGame();
-    }
-}
 
 
 
