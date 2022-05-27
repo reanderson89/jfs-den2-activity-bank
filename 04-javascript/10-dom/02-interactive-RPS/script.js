412551768c3c159d4f863f0e37eae70f47cc478c
robAnderson <reanderson89@gmail.com>
2022-05-27 15:13:29 -0700

    Done

--- 04-javascript/10-dom/02-interactive-RPS/script.js
+++ 04-javascript/10-dom/02-interactive-RPS/script.js
@@ -1,11 +1,12 @@
+
+
 const userObj = {
-    name: "Robert",
     wins: 0,
     losses: 0,
     ties: 0,
     _choice: "",
     askUserChoice(){
-        let userChoice = prompt("please choose 'r' for rock, 'p' for paper, or 's' for scissors");
+        let userChoice = "";
         this._choice = userChoice; 
         return userChoice;
     }
@@ -34,41 +35,39 @@ const compObj = {
 }
 
 const gameObj = {
-    gameCounter:1,
-    gamesToPlay: 2,
+    gameCounter:0,
     playGame(){
         this.gameCounter++;
         let userChoice = userObj.askUserChoice();
     let computerChoice = compObj.generateCompChoice();
-    alert(`You chose: ${userChoice}\nComputer chose: ${computerChoice}`)
 
     if ((userChoice === "r" && computerChoice === "s") ||
             (userChoice === "s" && computerChoice === "p") || 
             (userChoice === "p" && computerChoice === "r")) {
             userObj.wins++;
             compObj.losses++;
-            alert("You won!")
           } else if (userChoice === computerChoice) {
             userObj.ties++;
             compObj.ties++;
-            alert("You tied...")
           } else {
             userObj.losses++;
             compObj.wins++;
-            alert("womp womp womp...")
           }
 
-    if(this.gameCounter <= this.gamesToPlay){
-        this.playGame();
-    } else{
-        alert(`Final Standings:
-    ${userObj.name} won ${userObj.wins} time(s) and the computer won ${compObj.wins} time(s).
-    ${userObj.name} lost ${userObj.losses} time(s) and the computer lost ${compObj.losses} time(s).
-    There was ${userObj.ties} tie(s).
-    ${(userObj.wins > compObj.wins ? `${userObj.name} Wins!`: `The Computer Wins!`)}`)
+    //     alert(`Current Standings:
+    // You won ${userObj.wins} time(s) and the computer won ${compObj.wins} time(s).
+    // You lost ${userObj.losses} time(s) and the computer lost ${compObj.losses} time(s).
+    // There was ${userObj.ties} tie(s).
+    // ${(userObj.wins > compObj.wins ? `You Win!`: `The Computer Wins!`)}`)
+
+    // if(confirm("Play again?")){
+    //     this.playGame();
+    // }
     }
-    }
+
+    
 }
+
 
 
 
