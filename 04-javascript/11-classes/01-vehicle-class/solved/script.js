26107566206b83fe9b9f21cd653a42d95f6fbbeb
robAnderson <reanderson89@gmail.com>
2022-05-31 15:27:29 -0700

    solved 04-11-01

---
+++ 04-javascript/11-classes/01-vehicle-class/solved/script.js
@@ -0,0 +1,27 @@
+// Create a class called Vehicle
+// This class should have 4 properties that are general to all vehicles. Example: type, hasMotor, color, weight
+// The class should also have 2 methods, one method that checks if the vehicle has a motor, if it does it should return "vroom vroom"
+// The second method is up to you. Could be a method that returns the color and weight of the vehicle
+// Create 2 objects from your Vehicle class
+
+class Vehicle {
+    constructor(type, hasMotor, color, weight){
+        this.type = type;
+        this.hasMotor = hasMotor;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    checkForEngine(){
+        return this.hasMotor ? 'Vroom Vroom' : "Flintstones Time!"
+    }
+
+    aboutVehicle(){
+        return `This is a ${this.color} ${this.type}, and it weights ${this.weight}`
+    }
+}
+
+const hondaCivic = new Vehicle("Sedan", true, "Blue", 1200);
+
+console.log(hondaCivic);
+console.log(hondaCivic.checkForEngine());
