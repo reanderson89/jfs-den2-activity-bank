3f54f4b1ac8bb6a93dbc7e822329b996cfd1d28a
robAnderson <reanderson89@gmail.com>
2022-06-01 11:25:43 -0700

    added ch-4

---
+++ 04-javascript/00-Supplemental/js-1-js-2-assessment/challenge4.js
@@ -0,0 +1,5 @@
+// A leap year is a year containing one additional day added to keep the calendar year synchronized with the astronomical or seasonal year. Because seasons and astronomical events do not repeat in a whole number of days, calendars that have the same number of days in each year drift over time with respect to the event that the year is supposed to track. By inserting an additional day or month into the year, the drift can be corrected. A year that is not a leap year is called a common year.
+// Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial (years divisible by 100) years are leap years if they are exactly divisible by 400. For example, the years 1700, 1800, and 1900 are not leap years, but the year 2000 is.
+// Write a function named isLeapYear with a single parameter named year, that returns true if the year input is a leap year, and false if the year input is a common year.
+// For Example:
+//  When isLeapYear(2000) is called, it should return the value true. 
