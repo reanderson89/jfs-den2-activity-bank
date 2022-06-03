6a9f43603bbc1253a43644af42fd345c17f2fd6b
robAnderson <reanderson89@gmail.com>
2022-06-03 15:19:36 -0700

    finished display user setup

--- 04-javascript/14-requests/03-bad-request/solved/script.js
+++ 04-javascript/14-requests/03-bad-request/solved/script.js
@@ -1,9 +1,15 @@
 const makeRequest = async () => {
     let response = await fetch("https://reqres.in/api/users/23");
+    // if the response is bad
+    if(!response.ok){
+        throw new Error(`There is an error with status ${response.status}`)
+    }
     let usersJson = response.json();
     return usersJson;
 }
 
 makeRequest().then(users => {
     console.log(users);
+}).catch(error => {
+    console.log(error);
 })
