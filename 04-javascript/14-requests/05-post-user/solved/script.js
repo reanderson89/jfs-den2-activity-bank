813e9b7309600973ae83988743beab307d2471a5
robAnderson <reanderson89@gmail.com>
2022-06-06 15:39:37 -0700

    solved folder for post-users

--- 04-javascript/14-requests/05-post-user/solved/script.js
+++ 04-javascript/14-requests/05-post-user/solved/script.js
@@ -1,23 +1,46 @@
-const postUsers = () => {
+// DOM variables
+let submitBtn = document.getElementById("submit")
+// two inputs to grab users info
+let empName = document.getElementById("name")
+let empJob = document.getElementById("job")
+// place to display message for response
+let displayMessage = document.getElementById("message")
+
+const postUsers = (emp) => {
     return fetch("https://reqres.in/api/users", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
           },
-        body: JSON.stringify({
-            name: "Susan",
-            job: "Teacher"
-        })
+        body: JSON.stringify(emp)
     })
 }
 
 
+const addEmp = async () => {
+    let empObj = {name: empName.value, job: empJob.value}
+    console.log(empObj);
+    let response = await postUsers(empObj);
+    if(response.ok){
+        let data = await response.json();
+        console.log(data);
+        displayMessage.innerHTML = `${data.name} was added to the directory.`
+    } else {
+        displayMessage.innerHTML = "There was an error adding your employee, please try again."
+    }
 
-postUsers().then(response => {
-    return response.json()
-}).then(data => {
-    console.log(data);
-})
+}
+
+submitBtn.addEventListener("click", addEmp);
+
+
+// postUsers().then(response => {
+//     return response.json()
+// }).then(data => {
+//     console.log(data);
+// })
+
+
 
 
 // let person = {
