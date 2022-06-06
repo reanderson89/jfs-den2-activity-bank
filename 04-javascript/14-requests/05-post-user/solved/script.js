53478cb4cc9d7bc79d86037c25efeeccbe3f11b0
robAnderson <reanderson89@gmail.com>
2022-06-06 11:01:38 -0700

    solved 05-post-user

---
+++ 04-javascript/14-requests/05-post-user/solved/script.js
@@ -0,0 +1,48 @@
+const postUsers = () => {
+    return fetch("https://reqres.in/api/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+            name: "Ted",
+            job: "Coach"
+        })
+    })
+}
+
+
+
+postUsers().then(response => {
+    return response.json()
+}).then(data => {
+    console.log(data);
+})
+
+
+// let person = {
+//     name: "Ted",
+//     job: "Coach"
+// }
+// console.log(person);
+// console.log(typeof person);
+
+// // turns an object into a string
+// let personJSON = JSON.stringify(person);
+// console.log(personJSON);
+// console.log(typeof personJSON);
+
+// // turns a string back into an object
+// let personUnStringed = JSON.parse(personJSON);
+
+// console.log(personUnStringed);
+
+
+
+
+// Full "CRUD"
+
+// Create: POST
+// Read: GET
+// Update: PUT
+// Delete: DELETE
