ae9d30a967cd4ab384bd05ace24168eff6ecf6d3
robAnderson <reanderson89@gmail.com>
2022-06-09 07:54:35 -0700

    WIP, display current cit on page load

--- 04-javascript/14-requests/06-weather-api/solved/script.js
+++ 04-javascript/14-requests/06-weather-api/solved/script.js
@@ -3,7 +3,6 @@ const submitBtn = document.getElementById("submitBtn");
 const currentWeather = document.getElementById("weather");
 const cityBtns = document.getElementById("bonus-buttons");
 
-
 const cityStorage = {
     currentCity: "",
     cityBtnArr: [],
@@ -18,7 +17,6 @@ const cityStorage = {
     },
     getCurrentCity(){
         this.currentCity = localStorage.getItem("current-city")
-        displayWeather();
         return this.currentCity;
     },
     getCityArr(){
@@ -28,15 +26,11 @@ const cityStorage = {
 
 }
 
-
-
-
 const getWeather = (city) => {
     return fetch(`https://api.weatherapi.com/v1/current.json?key=46e1df487c204aaea80231816211312&q=${city}`)
 }
 
 const displayWeather = async () => {
-
     let currentCity = "";
     if(userCity.value){
         cityStorage.setCurrentCity(userCity.value);
@@ -60,12 +54,18 @@ const displayWeather = async () => {
 
 }
 
-
-cityStorage.getCurrentCity();
-cityStorage.getCityArr();
+const pageLoad = () => {
+   const currentCity =  cityStorage.getCurrentCity();
+   const cityArr =  cityStorage.getCityArr();  
+    if(currentCity){
+        displayWeather(currentCity);
+    }
+}
 
 ['click','submit'].forEach( evt => 
     submitBtn.addEventListener(evt, displayWeather)
 );
+
+pageLoad();
 
 
